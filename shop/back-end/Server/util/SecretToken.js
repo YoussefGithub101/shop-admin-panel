ca6d255ad4f775e36daa8857695ed3569e3f50db
YoussefGitHub101 <yousseffredy10@gmail.com>
2023-10-19 15:44:31 +0300

    test

--- shop/back-end/Server/util/SecretToken.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const jwt = require("jsonwebtoken");
-
-module.exports.createSecretToken = (user) => {
-  return jwt.sign({ user }, process.env.tokenKey, {
-    expiresIn: 3 * 24 * 60 * 60,
-  });
-};
